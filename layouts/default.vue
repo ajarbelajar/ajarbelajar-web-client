6098aee9469bb9ea2fd2e3ae56145f6b44bce50b
Dede Ariansya <dedeariansya1@gmail.com>
2020-08-17 19:21:22 +0800

    add footer and fix z-index

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,10 @@
   <div class="app-layout">
     <app-header></app-header>
     <app-sidebar></app-sidebar>
-    <Nuxt />
+    <div class="content-wrap">
+      <Nuxt />
+    </div>
+    <app-footer />
   </div>
 </template>
 
@@ -12,8 +15,13 @@
   flex-direction: column;
   min-height: 100vh;
   width: 100%;
-  padding-top: 60px;
+  padding-top: 60px + remToPx($spacer/2);
   padding-left: 240px;
   background-color: $gray-100;
+
+  @include media-breakpoint-down(md) {
+    transition: padding-left 400ms cubic-bezier(0.785, 0.135, 0.15, 0.86);
+    padding-left: 0;
+  }
 }
 </style>
