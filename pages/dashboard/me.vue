5032e570425b71ecf73d19182b03a2d292e7deab
dede ariansya <dedeariansya1@gmail.com>
2021-07-27 06:24:29 +0800

    #avatar

--- pages/dashboard/me.vue
+++ pages/dashboard/me.vue
@@ -2,8 +2,15 @@
   <div class="py-3">
     <div class="overflow-hidden rounded-lg border">
       <div class="flex py-5 px-4">
-        <div class="p-1 w-16 rounded-full border">
+        <div class="p-1 w-16 rounded-full border relative">
           <v-img class="block w-full rounded-full" :src="auth.avatar" :src-placeholder="$images.avatar" />
+          <div class="bottom-0 left-0 flex justify-center transform translate-y-2/4 w-full absolute">
+            <button @click.prevent="handleClick" type="button" :disabled="loading" class="flex text-primary-600 w-7 h-7 rounded-full bg-white border hover:bg-gray-200 justify-center items-center">
+              <svg-spinner v-if="loading" class="w-4" />
+              <i class="ft ft-image" v-else />
+            </button>
+            <input type="file" @change="handleChange" class="hidden" ref="input" accept="image/*" />
+          </div>
         </div>
         <div class="flex-1 pl-4 my-auto">
           <h2 class="truncate text-lg font-semibold leading-none">{{ auth.name }}</h2>
@@ -35,9 +42,15 @@
 </template>
 
 <script>
+import FormData from 'form-data'
 export default {
   name: "MyDashboard",
   middleware: 'auth',
+  data() {
+    return {
+      loading: false
+    }
+  },
   computed: {
     auth() {
       return this.$store.getters.auth
@@ -66,6 +79,25 @@ export default {
         },
       ]
     }
-  }
+  },
+  methods: {
+    handleClick() {
+      this.$refs.input.click()
+    },
+    async handleChange(ev) {
+      this.loading = true
+      try {
+        const file = ev.target.files[0]
+        const data = new FormData()
+        data.append('avatar', file)
+        const { url } = await this.$axios.$post('/account/avatar', data)
+        this.$store.commit('auth/setAuth', { ...this.auth, avatar: url })
+        this.$toast.success('Foto anda telah diupdate.')
+      } catch (e) {
+        this.$toast.danger(this.$errorMessage(e))
+      }
+      this.loading = false
+    },
+  },
 }
 </script>
