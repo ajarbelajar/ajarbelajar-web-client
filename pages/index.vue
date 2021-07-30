e8dbca4ad1acd4244afce5c1562b10eca3b60aea
dede ariansya <dedeariansya1@gmail.com>
2021-07-31 03:50:13 +0800

    #landingInfo

--- pages/index.vue
+++ pages/index.vue
@@ -2,9 +2,8 @@
   <main class="bg-white">
     <landing-navbar />
     <landing-header />
-    <div class="py-6" />
     <landing-counter :count="{ user: 600, minitutor: 120, video: 120, article: 40 }" />
-    <div class="py-6" />
+    <landing-info />
   </main>
 </template>
 
