c7f00777c4c10abde6f821fd5b27f1191fddee11
dede ariansya <dedeariansya1@gmail.com>
2022-03-27 22:21:43 +0800

    fix text truncate

--- components/elements/UserList.vue
+++ components/elements/UserList.vue
@@ -1,5 +1,5 @@
 <template>
-  <nuxt-link :to="`/users/${user.username}`" class="group p-2 bg-white rounded shadow hover:shadow-lg">
+  <nuxt-link :to="`/users/${user.username}`" class="group block p-2 bg-white rounded shadow hover:shadow-lg">
     <div class="flex relative items-center">
       <span class="text-2xs bg-primary-100 text-primary-600 block absolute -top-1 -right-1 px-2 font-bold rounded-full">{{ user.points }} POIN</span>
       <div class="flex flex-col justify-center items-center">
@@ -7,9 +7,9 @@
           <v-img class="w-full rounded-full" :src="user.avatar || $images.avatar" :src-placeholder="$images.avatar" />
         </div>
       </div>
-      <div class="flex-1 pl-2">
+      <div class="truncate flex-1 pl-2">
         <h3 class="truncate mb-1 text-sm font-semibold leading-none capitalize">{{ user.name }}</h3>
-        <p class="text-xs leading-none text-gray-600 capitalize" >@{{ user.username }}</p>
+        <p class="truncate text-xs leading-none text-gray-600 capitalize" >@{{ user.username }}</p>
       </div>
     </div>
   </nuxt-link>
